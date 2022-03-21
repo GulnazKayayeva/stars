8264a634b8ae1db0af854f3337831ca0ff323305
GulnazKayayeva <yuupuu579@gmail.com>
2022-03-21 17:35:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.write(`Первое задание:`);
+
+document.write(`<br><br>`);
+
+for(let i=1;i<=9;i++){
+    for(let k=1;k<=3;k++){
+        document.write(i)
+    }
+}
+
+document.write(`<br><br><br>`);
+
+
+document.write(`Второе задание:`);
+document.write(`<br><br>`);
+for(let i=5;i>=1;i--){
+    for(let k=1;k<=i;k++){
+        document.write(`*`)
+    }
+document.write(`<br>`)
+        
+    }
+    
